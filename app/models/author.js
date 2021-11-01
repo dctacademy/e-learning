d5f30b4fbec767ea3c5e3ad59c357a9556a6cbb2
aniruddha <ani@dctacademy.com>
2021-11-01 17:20:14 +0530

    added author bio

--- app/models/author.js
+++ app/models/author.js
@@ -7,6 +7,9 @@ const authorSchema = new Schema({
         minlength: [4, 'username must be minimum 4 characters long'],
         maxlength: [64, 'username should not be more than 64 characters long'],
         unique: true
+    },
+    bio: {
+        type: String
     }
 }, { timestamps: true })
 
